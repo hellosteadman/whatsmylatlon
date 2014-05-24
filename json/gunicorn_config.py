3c80c623f7da176a899b75fbd0c7b844a4b9c741
Mark Steadman <marksteadman@me.com>
2014-05-24 22:58:29 +0100

    Updated unicorn config

--- json/gunicorn_config.py
+++ json/gunicorn_config.py
@@ -1,5 +1,5 @@
 command = '/opt/env/latlon.steadman.io/bin/gunicorn'
-pythonpath = '/opt/env/latlon.steadman.io/latlon.steadman.io'
+pythonpath = '/opt/env/latlon.steadman.io/latlon.steadman.io/json'
 bind = '127.0.0.1:8003'
 workers = 3
 user = 'latlon'
